82f8773dc22c6d05b5a8958e00b9920efbb0aa81
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-05-01 23:21:02 +0200

    Adding a .sh setup file for emacs for convenience

---
+++ setup-emacs.sh
@@ -0,0 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#just copy the init file and the emacs.d in the home directory of the computer
+cp $DIR/.emacs  ~/.emacs -b 
+cp $DIR/.emacs.d  ~/.emacs.d/ -b -R
