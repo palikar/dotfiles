7d07fb548fcb613855837d7344b4b56a0d567df4
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-02-21 23:12:35 +0100

    Cleaning ranger a little bit

--- .config/ranger/plugins/devicons_linemode.py
+++ .config/ranger/plugins/devicons_linemode.py
@@ -1,19 +1,19 @@
-# import ranger.api
-# from ranger.core.linemode import LinemodeBase
-# from devicons import *
+import ranger.api
+from ranger.core.linemode import LinemodeBase
+from plugins.devicons import *
 
-# @ranger.api.register_linemode
-# class DevIconsLinemode(LinemodeBase):
-#   name = "devicons"
+@ranger.api.register_linemode
+class DevIconsLinemode(LinemodeBase):
+  name = "devicons"
 
-#   uses_metadata = False
+  uses_metadata = False
 
-#   def filetitle(self, file, metadata):
-#     return devicon(file) + ' ' + file.relative_path
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
 
-# @ranger.api.register_linemode
-# class DevIconsLinemodeFile(LinemodeBase):
-#   name = "filename"
+@ranger.api.register_linemode
+class DevIconsLinemodeFile(LinemodeBase):
+  name = "filename"
 
-#   def filetitle(self, file, metadata):
-#     return devicon(file) + ' ' + file.relative_path
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
