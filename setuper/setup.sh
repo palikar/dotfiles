1f5cb7543221f7443748c398a4a73cd8942ee49f
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-03-09 12:20:32 +0100

    Adding some conf files for the scripts in the system

--- setuper/setup.sh
+++ setuper/setup.sh
@@ -4,9 +4,9 @@
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 DIR=$SCRIPTDIR/..
 
-$CODE=$HOME/code
-$CODE_EXT=$HOME/code_ext
-$CODE_SYS=$HOME/code_sys
+CODE="$HOME/code"
+CODE_EXT="$HOME/code_ext"
+CODE_SYS="$HOME/code_sys"
 
 
 create_home_dirs()
@@ -20,23 +20,21 @@ create_home_dirs()
 install_deb_packages()
 {
 	sudo apt-get update
-	sudo apt-get upgrade
-	sudo apt-get update
-
+	
 	for pack in $(cat "$SCRIPTDIR/apt_packages"); do
 		sudo apt-get install -y --force-yes $pack
 	done
 
-	sudo apt-get update
-	sudo apt-get upgrade
-	sudo apt-get update
+	sudo apt-get update -y --force-yes
+	sudo apt-get upgrade -y --force-yes
+	sudo apt-get update -y --force-yes
 	sudo apt autoremove
 }
 
 install_python_packages()
 {
 	for pack in $(cat "$SCRIPTDIR/python_packages"); do
-        pip3 install $pack
+        pip3 install $pack --user
     done
 }
 
@@ -48,7 +46,7 @@ pull_all()
         $(git pull "$repo" 2> /dev/null) || echo "$repo is already there"
 	done
 
-	cd -
+	cd ~
 }
 
 set_package_sources()
@@ -98,22 +96,72 @@ setup_files()
 
 setup_vim()
 {
+	[ -d "$HOME/.vim_runtime" ] || rm -rf "$HOME/.vim_runtime"
 	git clone --depth=1 https://github.com/amix/vimrc.git "$HOME/.vim_runtime"
-	sh "$HOME/.vim_runtime/install_basic_vimrc.sh"
+	sh ~/.vim_runtime/install_awesome_vimrc.sh
 }
 
 setup_fzf()
 {
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install	
+	[-d "$HOME/.fzf" ] || rm -rf "$HOME/.fzf"
+	git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+	$HOME/.fzf/install --key-bindings --no-completion --update-rc
 }
 
 setup_emacs()
 {
 	cd $CODE_EXT
 	wget "https://ftp.gnu.org/gnu/emacs/emacs-26.1.tar.gz"
-	tar jxvf "emacs-26.1.tar.gz"
-	cd -
+	tar zxvf "emacs-26.1.tar.gz"
+	cd ./emacs-26.1
+	autoreconf --force --install
+	export CFLAGS="-g3 -O3" && ./configure --with-x  --with-xwidgets --with-mailutils --with-pop
+	make -j4 && sudo make install
+	cd $HOME
+}
+
+setup_i3()
+{
+	cd $CODE_EXT
+	cd ./i3
+	autoreconf --force --install
+	[ -d ./build ] && rm -rf build/
+	mkdir -p build && cd build/
+	echo "$(pwd)"
+	export CFLAGS="-g3 -O3"
+	../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+	make -j4 && sudo make install
+	cd $HOME
+}
+
+setup_urxvt()
+{
+	cd $CODE_EXT
+	wget "http://dist.schmorp.de/rxvt-unicode/Attic/rxvt-unicode-9.22.tar.bz2"
+	tar jxvf "rxvt-unicode-9.22.tar.bz2"
+	cd ./rxvt-unicode-9.22
+	autoreconf --force --install
+	export CFLAGS="-g3 -O3" &&   ./configure --enable-everything --enable-256-color --prefix=/usr
+	make -j4 && sudo make install
+	cd $HOME
+}
+
+setup_dropbox()
+{
+	cd $HOME/Downloads/
+	wget -O "dropbox.deb" "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.01.31_amd64.deb"
+	sudo dpkg -i "dropbox.deb"
+	sudo apt-get -f install
+	cd $HOME
+}
+
+setup_discrod()
+{
+	wget -O "discord.deb" "https://discordapp.com/api/download?platform=linux&format=deb"
+	sudo dpkg -i "discord.deb"
+	sudo apt-get -f install
+	cd $HOME
+
 }
 
 [ -f $SCRIPTDIR/apt_packages ] || (echo "The apt packages file is not there!" && exit 1)
@@ -124,6 +172,8 @@ setup_emacs()
 [ -f $SCRIPTDIR/codes ] || ( echo "The codes file is not there!" && exit 1)
 
 
+# Setup basic system
+
 create_home_dirs
 
 set_package_sources
@@ -132,7 +182,8 @@ install_deb_packages
 
 install_python_packages
 
-create_home_dirs
+
+# Setup basic application
 
 setup_files
 
@@ -140,7 +191,7 @@ setup_vim
 
 setup_fzf
 
-setup_emacs
+# Pull repos from github
 
 pull_all $CODE "$SCRIPTDIR/codes"
 
@@ -149,7 +200,17 @@ pull_all $CODE_EXT "$SCRIPTDIR/ext_codes"
 pull_all $CODE "$SCRIPTDIR/sys_codes"
 
 
+# Setup essenital applications
 
+setup_i3
+
+setup_urxvt
+
+setup_emacs
+
+setup_dropbox
+
+setup_discrod
 
 
 source "${HOME}/.profile"
